fee4c42027497b47b878e349b75b1d7fad435ba6
AMAL M REJI <88614402+Amalreji111@users.noreply.github.com>
2022-01-26 23:42:09 +0530

    Initial Tindog Project

---
+++ demo.js
@@ -0,0 +1,7 @@
+function alertDemo(){
+alert("This site is just for educational purpose,So you can't make any payment on this site ");
+
+}
+function alertForDownload(){
+    alert("All apps mentioned in this site are just used for educational purpose,You need not be download any of app ,If you download then it is your risk");
+}
